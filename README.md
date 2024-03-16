ec01f81ee77f42ff4be965c418429730ee3e4a85
Mohamed Aly Ahmed <MohamedAlyAhmed@aol.com>
2024-03-17 00:33:44 +0200

    Initialize app

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mekasel App
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@styles/index.scss";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <h1>Mekasel | eCommerce</h1>
+    </React.StrictMode>
+);
