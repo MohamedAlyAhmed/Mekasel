87be281cdda9a5880a8fb67e0b30ca82b5e3c7c2
Mohamed Aly Ahmed <MohamedAlyAhmed@aol.com>
2024-07-05 03:55:24 +0300

    feat: customized error page

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,25 +1,38 @@
 import Container from "@components/common/Container/Container";
 import Wrapper from "@components/common/Wrapper/Wrapper";
 import { appPaths } from "@routes/paths";
-import { Link } from "react-router-dom";
+import { isRouteErrorResponse, Link, useRouteError } from "react-router-dom";
 import styles from "./Error.module.scss";
 
 const { container } = styles;
 
 const Error = () => {
-  return (
-    <Container>
-      <Wrapper>
-        <div className={container}>
-          <h1>404</h1>
-          <p>Page Not Found</p>
-          <Link title="Go To Home" to={appPaths.home} replace={true}>
-            Go to Home
-          </Link>
-        </div>
-      </Wrapper>
-    </Container>
-  );
+    const error = useRouteError();
+
+    let errorStatus: string;
+    let errorStatusText: string;
+
+    if (isRouteErrorResponse(error)) {
+        errorStatus = `${error.status}`;
+        errorStatusText = error.statusText;
+    } else {
+        errorStatus = "404";
+        errorStatusText = "Page Not Found";
+    }
+
+    return (
+        <Container>
+            <Wrapper>
+                <div className={container}>
+                    <h1>{errorStatus}</h1>
+                    <p>{errorStatusText}</p>
+                    <Link title="Go To Home" to={appPaths.home} replace={true}>
+                        Go to Home
+                    </Link>
+                </div>
+            </Wrapper>
+        </Container>
+    );
 };
 
 export default Error;
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -9,57 +9,69 @@ import Login from "@pages/Login";
 import Products from "@pages/Products";
 import Register from "@pages/Register";
 import Wishlist from "@pages/Wishlist";
+import { appPaths } from "@routes/paths";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { appPaths } from '@routes/paths';
 
 const router = createBrowserRouter([
-  {
-    path: appPaths.home,
-    element: <MainLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: appPaths.login,
-        element: <Login />,
-      },
-      {
-        path: appPaths.register,
-        element: <Register />,
-      },
-      {
-        path: appPaths.categories,
-        element: <Categories />,
-      },
-      {
-        path: appPaths.products,
-        element: <Products />,
-      },
-      {
-        path: appPaths.aboutUS,
-        element: <AboutUs />,
-      },
-      {
-        path: appPaths.contactUs,
-        element: <ContactUs />,
-      },
-      {
-        path: appPaths.cart,
-        element: <Cart />,
-      },
-      {
-        path: appPaths.wishlist,
-        element: <Wishlist />,
-      },
-    ],
-  },
+    {
+        path: appPaths.home,
+        element: <MainLayout />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: appPaths.login,
+                element: <Login />,
+            },
+            {
+                path: appPaths.register,
+                element: <Register />,
+            },
+            {
+                path: appPaths.categories,
+                element: <Categories />,
+            },
+            {
+                path: appPaths.products,
+                element: <Products />,
+                loader: ({ params }) => {
+                    if (
+                        typeof params.prefix !== "string" ||
+                        !/^[a-z]+$/.test(params.prefix)
+                    ) {
+                        throw new Response("Bad Prefix", {
+                            status: 400,
+                            statusText: "Category not found",
+                        });
+                    }
+                    return true;
+                },
+            },
+            {
+                path: appPaths.aboutUS,
+                element: <AboutUs />,
+            },
+            {
+                path: appPaths.contactUs,
+                element: <ContactUs />,
+            },
+            {
+                path: appPaths.cart,
+                element: <Cart />,
+            },
+            {
+                path: appPaths.wishlist,
+                element: <Wishlist />,
+            },
+        ],
+    },
 ]);
 
 const AppRouter = () => {
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />;
 };
 
 export default AppRouter;
